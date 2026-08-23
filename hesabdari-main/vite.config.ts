1162f19c2816a2587e383a22042461ba1e29facb
amirmhypr <amir.h.apps@gmail.com>
2026-08-23 16:08:23 +0330

    Change base path in Vite config

--- hesabdari-main/vite.config.ts
+++ hesabdari-main/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/hesabdari/',
+    base: '/-hesabdari/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
